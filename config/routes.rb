6caa9d4cc990462e698d7456b1a8dfe14642045a
marviorocha <marviorocha@marviorocha.com>
2021-08-13 09:47:57 -0300

    Routes api setting rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+  namespace :api do
+    namespace :v1, default: {format: :json} do
+  
+      
+    end
+  end
+
+
 end
